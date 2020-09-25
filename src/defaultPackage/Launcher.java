6610654cf3852e6a77b82e58df65e6d4459a255a
klevvit <33697374+klevvit@users.noreply.github.com>
2020-09-25 14:04:45 +0300

    Copyright correction

--- src/defaultPackage/Launcher.java
+++ src/defaultPackage/Launcher.java
@@ -1,9 +1,9 @@
+package defaultPackage;
+
 /**
  * @author  Kovalenko Lev
  * Copyright © Kovalenko Lev (Sweeper) 2016-2020. All rights reserved.
  */
-package defaultPackage;
-
 public class Launcher {  // TODO deal with copyright
 
 	public static void main(String[] args) {
